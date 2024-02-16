397f7e481c5b80bc03b9089d502f82c9f4c8485a
s_zervos <s_zervos@wargaming.net>
2024-02-16 15:42:13 +0200

    Making the Mobile Navigation Work

--- Tutorial/09-Omnifood-Optimizations/js/script.js
+++ Tutorial/09-Omnifood-Optimizations/js/script.js
@@ -1,5 +1,14 @@
+/** SET CURRENT YEAR */
 const yearEl = document.querySelector(".year");
 yearEl.textContent = new Date().getFullYear();
+
+/** MAKE MOBILE NAVIGATION */
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
