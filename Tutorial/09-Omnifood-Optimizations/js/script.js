5cfd02e2e271964d2bd32cb5766cd84e1aa1cd3e
s_zervos <s_zervos@wargaming.net>
2024-02-16 16:04:15 +0200

    Implementing Smooth Scrolling

--- Tutorial/09-Omnifood-Optimizations/js/script.js
+++ Tutorial/09-Omnifood-Optimizations/js/script.js
@@ -9,6 +9,35 @@ const headerEl = document.querySelector(".header");
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+
+/** SMOOTH SCROLLING ANIMATION */
+/** For this part to work add https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js as HTML script in the index.html*/
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault; // prevents default function of the link when it is clicked
+    const href = link.getAttribute("href");
+
+    /** Scroll back to top */
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+
+    /** Scroll to section */
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    /** Close mobile navigation */
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  });
+});
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
@@ -28,5 +57,3 @@ function checkFlexGap() {
   if (!isSupported) document.body.classList.add("no-flexbox-gap");
 }
 checkFlexGap();
-
-// https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
