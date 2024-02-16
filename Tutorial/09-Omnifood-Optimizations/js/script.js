e0512dde24634d18061c678be0a73b00912358e1
s_zervos <s_zervos@wargaming.net>
2024-02-16 16:33:05 +0200

    Implementing a Sticky Navigation Bar

--- Tutorial/09-Omnifood-Optimizations/js/script.js
+++ Tutorial/09-Omnifood-Optimizations/js/script.js
@@ -38,6 +38,27 @@ allLinks.forEach(function (link) {
     }
   });
 });
+
+/** STICKY NAVIGATION */
+const sectionHeroEl = document.querySelector(".section-hero");
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0, //whenever the section-hero is completely out of the viewport
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
